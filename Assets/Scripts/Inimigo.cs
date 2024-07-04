4b9edb34577e202dd41b77f092743aebaf17ccc6
phns2233 <nerisdesousa2020@gmail.com>
2024-07-04 18:02:36 -0300

    t

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -7,7 +7,7 @@ public class Inimigo : MonoBehaviour
     
     public Transform player; // Referência ao transform do jogador
     public float moveSpeed = 3f; // Velocidade de movimento do inimigo
-    public float stopDistance = 1f; // Distância a partir da qual o inimigo para de seguir o jogador
+    public float stopDistance = 5f; // Distância a partir da qual o inimigo para de seguir o jogador
     public SpriteRenderer spriteR;
     public Animator anim;
     private bool isFollowing = true; // Variável de controle do estado de seguir
@@ -30,12 +30,12 @@ public class Inimigo : MonoBehaviour
             // Move o inimigo na direção calculada
             rb.velocity = moveDirection * moveSpeed;
 
-            // Ajusta a rotação do inimigo para olhar na direção do movimento
+           /* // Ajusta a rotação do inimigo para olhar na direção do movimento
            if (moveDirection != Vector2.one)
             {
                 float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            }
+            }*/
 
             // Verifica se o inimigo está próximo o suficiente para parar de seguir
             if (Vector2.Distance(transform.position, player.position) <= stopDistance)
